a8f7da4b1452c1162d95579f0a65bd9f7407ae45
KV Le <32186663+kvietcong@users.noreply.github.com>
2022-07-30 20:24:50 -0500

    ✨ Combine best of both worlds and 🖼 Add buffer canvas
    Thank you Hung and Angelo!

--- script.js
+++ script.js
@@ -8,9 +8,12 @@ const canvas = document.getElementById("starfield");
 /** The canvas context for drawing */
 const context = canvas.getContext("2d");
 /** Stars per square pixel */
-const starDensity = 1 / 16_000;
+const starDensity = 1 / 10_000;
 /** Flicker rate */
-const flickerRate = 5;
+const flickerRate = 2;
+
+const bufferCanvas = document.createElement("canvas");
+const bufferContext = bufferCanvas.getContext("2d");
 
 const randomBinomial = (min, max, skew) => {
     let u = 0, v = 0;
@@ -42,27 +45,29 @@ const Star = {
     clone: star => ({...star}),
     draw: (star, isForced = false) => {
         if (!isForced && !Star.isInView(star)) return star;
-        context.fillStyle = `rgba(255, 255, 255, ${star.opacity})`;
+        bufferContext.fillStyle = `rgba(255, 255, 255, ${star.opacity})`;
 
-        context.beginPath(); {
-            context.arc(star.x, star.y, star.radius * 3, 0, 360);
+        bufferContext.beginPath(); {
+            bufferContext.arc(star.x, star.y, star.radius * 3, 0, 360);
 
-            context.moveTo(star.x - star.radius, star.y);
-            context.lineTo(star.x, star.y - star.radius * 15 * star.yMultiplier);
-            context.lineTo(star.x + star.radius, star.y);
+            if (star.radius > 0.3) {
+                bufferContext.moveTo(star.x - star.radius, star.y);
+                bufferContext.lineTo(star.x, star.y - star.radius * 15 * star.yMultiplier);
+                bufferContext.lineTo(star.x + star.radius, star.y);
 
-            context.moveTo(star.x, star.y - star.radius);
-            context.lineTo(star.x + star.radius * 15 * star.xMultiplier, star.y);
-            context.lineTo(star.x, star.y + star.radius);
+                bufferContext.moveTo(star.x, star.y - star.radius);
+                bufferContext.lineTo(star.x + star.radius * 15 * star.xMultiplier, star.y);
+                bufferContext.lineTo(star.x, star.y + star.radius);
 
-            context.moveTo(star.x + star.radius, star.y);
-            context.lineTo(star.x, star.y + star.radius * 15 * star.yMultiplier);
-            context.lineTo(star.x - star.radius, star.y);
+                bufferContext.moveTo(star.x + star.radius, star.y);
+                bufferContext.lineTo(star.x, star.y + star.radius * 15 * star.yMultiplier);
+                bufferContext.lineTo(star.x - star.radius, star.y);
 
-            context.moveTo(star.x, star.y + star.radius);
-            context.lineTo(star.x - star.radius * 15 * star.xMultiplier, star.y);
-            context.lineTo(star.x, star.y - star.radius);
-        } context.fill();
+                bufferContext.moveTo(star.x, star.y + star.radius);
+                bufferContext.lineTo(star.x - star.radius * 15 * star.xMultiplier, star.y);
+                bufferContext.lineTo(star.x, star.y - star.radius);
+            }
+        } bufferContext.fill();
 
         return star;
     },
@@ -70,12 +75,12 @@ const Star = {
         if (!isForced && !Star.isInView(star)) return star;
         const newStar = Star.clone(star);
         newStar.change *= (newStar.opacity >= 1 || newStar.opacity <= 0) ? -1 : 1;
-        newStar.opacity += newStar.change * Math.random() / 100 * flickerRate;
+        newStar.opacity += newStar.change * (Math.random() + 0.1) / 100 * flickerRate;
         return newStar;
     },
     parallax: (star, movement, isForced = false) => {
         if (!isForced && !Star.isInView(star)) return star;
-        const random = Math.random() / 135;
+        const random = Math.random() / 175;
         const newStar = Star.clone(star);
         newStar.x += random * movement.x;
         newStar.y += 1.5 * random * movement.y;
@@ -89,15 +94,8 @@ const Star = {
     },
 };
 
-/** Resizes the canvas and re-initializes the stars */
-const resize = () => {
-    canvas.width = document.body.clientWidth;
-    canvas.height = document.body.clientHeight;
-    init();
-}
-
 /** General update function */
-const update = () => { stars = stars.map(Star.update) };
+const update = () => { stars = stars.map(star => Star.update(star)) };
 
 
 const circs = 20;
@@ -118,34 +116,40 @@ const rands = range(0, circs).map(_ => range(0, 3).map(__ => Math.random()));
 
 // METABALLS?
 const drawBackground = () => {
-    context.globalCompositeOperation = "lighten";
-    context.clearRect(0, 0, canvas.width, canvas.height);
     for (let i = 0; i < circs; i++) {
-        const x0 = rands[i][0] * canvas.width;
-        const y0 = rands[i][1] * canvas.height;
-        const r0 = rands[i][2] * 100 + 50 * (canvas.width * canvas.height / 50000000);
+        const x0 = rands[i][0] * bufferCanvas.width;
+        const y0 = rands[i][1] * bufferCanvas.height;
+        const r0 = rands[i][2] * 100 + 50 * (bufferCanvas.width * bufferCanvas.height / 50000000);
 
         const x1 = x0 + rands[i][1] * x0 / 50;
         const y1 = y0 + rands[i][1] * y0 / 50;
-        const r1 = r0 + rands[i][3] * 800 + 300;
+        const r1 = r0 + rands[i][3] * 500 + 500;
 
-        const gradient = context.createRadialGradient(x0, y0, r0, x1, y1, r1);
+        const gradient = bufferContext.createRadialGradient(x0, y0, r0, x1, y1, r1);
         gradient.addColorStop(0.0, "rgba(128, 0, 128, 0.2)");
         gradient.addColorStop(clamp(0.8 * rands[i][0] + Math.sin(new Date().getTime() / 50000) / 2, 0.10, 0.7), "rgba(25, 25, 112, 0.5)");
         gradient.addColorStop(0.75, "rgba(25, 25, 112, 0.25)");
         gradient.addColorStop(0.90, "rgba(25, 25, 112, 0.125)");
         gradient.addColorStop(1.0, "rgba(25, 25, 112, 0.0)");
-        context.fillStyle = gradient;
-        context.beginPath(); {
-            context.arc(x0, y0, r1, 0, Math.PI * 2);
-        } context.fill();
+        bufferContext.fillStyle = gradient;
+        bufferContext.beginPath(); {
+            bufferContext.arc(x0, y0, r1, 0, Math.PI * 2);
+        } bufferContext.fill();
     }
 }
 
 const draw = () => {
+    bufferCanvas.width = canvas.width;
+    bufferCanvas.height = canvas.height;
+
+    bufferContext.globalCompositeOperation = "lighten";
+    bufferContext.clearRect(0, 0, bufferCanvas.width, bufferCanvas.height);
     drawBackground();
-    stars.forEach(Star.draw);
+    stars.forEach(star => Star.draw(star));
     window.requestAnimationFrame(draw);
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(bufferCanvas, 0, 0);
 }
 
 /** Calculates and draws simple parallax effect */
@@ -164,7 +168,7 @@ const init = () => {
     for (let i = 0; i < starCount; i++) {
         const opacity = Math.random();
         // const radius = 3 * Math.random() * Math.random();
-        const radius = randomBinomial(0.32, 4, 5);
+        const radius = randomBinomial(0.1, 1.5, 3.5);
         const x = Math.random() * canvas.offsetWidth;
         const y = Math.random() * canvas.offsetHeight;
 
@@ -181,13 +185,13 @@ const init = () => {
 window.addEventListener("load", init);
 window.setInterval(update, 1000 / ups);
 window.requestAnimationFrame(draw);
-window.addEventListener("resize", resize);
-// window.addEventListener("mousemove", event => {
-//     parallax({
-//         x: event.movementX,
-//         y: event.movementY
-//     });
-// });
+window.addEventListener("resize", init);
+window.addEventListener("mousemove", event => {
+    parallax({
+        x: event.movementX,
+        y: event.movementY
+    });
+});
 
 // All anchor links will be smooth scrolled to
 document.querySelectorAll("a[href^='#']").forEach(anchor => {
